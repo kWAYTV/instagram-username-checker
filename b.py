0bcda470f8ef190e4c305c318be2572ff2daf1c0
kWAY <55437151+kWAYTV@users.noreply.github.com>
2022-08-16 13:31:24 +0200

    Update b.py

--- b.py
+++ b.py
@@ -5,6 +5,9 @@ from pystyle import Colors, Colorate, Center
 clear = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear") # Don't touch this
 users = open('check.txt', 'r').read().split('\n')
 count = 0
+free = 0
+taken = 0
+error = 0
 proxyDebug = False
 os.system(f"title Instagram Username Checker - Starting...")
 clear()
@@ -22,7 +25,7 @@ def printLogo():
         print(Center.XCenter(Colorate.Horizontal(Colors.white_to_green, logo, 1)))
 
 def check():
-    global count
+    global count, free, taken, error
     while True:
         for line in users:
             proxy = random.choice(open("proxies.txt","r").read().splitlines()); proxyDict = {"http": f"http://{proxy}"}
@@ -31,22 +34,23 @@ def check():
             else:
                 pass
             response = requests.get(f"https://www.instagram.com/{line}/", proxies=proxyDict)
+            count += 1
             if (response.status_code == 200):
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Taken: " + line)
                 with open ("taken.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
+                    taken += 1
             elif (response.status_code == 404):
                 print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}] {Fore.RESET}Free/Termed: "+ line)
                 with open ("free.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
+                    free += 1
             else:
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Error: " + line)
                 with open ("error.txt", "a") as f:
                     f.write(line + "\n")
-                    count += 1
-            os.system(f"title Instagram Username Checker - Checked {count}/{len(users)} - Remaining: {len(users)-count}")
+                    error += 1
+            os.system(f"title Instagram Username Checker - Checked {count}/{len(users)} - Free: {free} - Taken: {taken} - Error: {error} - Remaining: {len(users)-count}")
 
 
 clear()
